ce464c9a64fe9d4bff99cfc65cfb22834101fcfe
Diego Essaya <dessaya@gmail.com>
2017-05-21 20:19:52 -0300

    Send mail

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import traceback
 from collections import namedtuple
 import json
 from correctores import alumnos
+from google.appengine.api import mail, app_identity
 
 templates = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
@@ -44,24 +45,44 @@ class MainPage(webapp2.RequestHandler):
             if hasattr(f, 'filename')
         ]
 
+    def get_docentes(self, tp, padrones):
+        docentes = set(alumnos[p].get(tp, '') for p in padrones)
+        warning = None
+        if '' in docentes:
+            warning = u'No todos los integrantes tienen un docente asignado para el {}'.format(tp)
+        elif len(docentes) > 1:
+            warning = u'No hay un único docente asignado para el grupo.'
+        return [d for d in docentes if d], warning
+
+    def sendmail(self, docentes, tp, padrones, files, body):
+        mail.send_mail(
+            sender='Entregas Algoritmos 1 <{}@appspot.gserviceaccount.com>'.format(
+                app_identity.get_application_id()
+            ),
+            to=['tps.7540rw@gmail.com'],
+            subject=u'{} - {}'.format(tp, ' - '.join(padrones)),
+            body=body,
+            attachments=[
+                mail.Attachment(f.filename, f.content)
+                for f in files
+            ]
+        )
+
     def post(self):
         try:
             tp = self.request.POST.get('tp').upper()
             padrones = self.get_padrones()
             files = self.get_files()
-            body = self.request.POST.get('body')
+            body = self.request.POST.get('body') or ''
+            docentes, warning = self.get_docentes(tp, padrones)
 
-            docentes = set(alumnos[p].get(tp, '') for p in padrones)
-            warning = None
-            if '' in docentes:
-                warning = u'Algún(os) integrantes no tienen un docente asignado para el {}'.format(tp)
-            elif len(docentes) > 1:
-                warning = u'No hay un único docente asignado para el grupo.'
+            self.sendmail(docentes, tp, padrones, files, body)
+
             self.render('result.html', {
                 'warning': warning,
                 'sent': {
                     'tp': tp,
-                    'docentes': [d for d in docentes if d],
+                    'docentes': docentes,
                 }
             })
         except Exception as e:
